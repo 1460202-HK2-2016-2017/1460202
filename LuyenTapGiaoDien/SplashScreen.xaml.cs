7a7bf471e9dd43821559f2babd12fe8eb1a51257
1460202 <nguyenduy2496@outlook.com.vn>
2017-03-13 20:13:46 +0700

    source

---
+++ LuyenTapGiaoDien/SplashScreen.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Threading;
+
+namespace LuyenTapGiaoDien
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashScreen_Loaded(object sender, RoutedEventArgs e)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged+=worker_ProgressChanged;
+            worker.RunWorkerCompleted+=worker_RunWorkerCompleted;
+
+            worker.RunWorkerAsync();
+        }
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(30);
+            }
+            //Thread.ResetAbort();
+            
+        }
+        private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.Loading.Value = e.ProgressPercentage;
+        }
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var window = new MainWindow();
+            this.Hide();
+            window.Show();
+            this.Close();
+        }
+
+    }
+}
